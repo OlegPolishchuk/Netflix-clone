fd35a489bebd6a9adc77428a7628bc9d9b717d72
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-02-19 11:23:15 +0300

    feat: 9: add navbar styles

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,28 +1,60 @@
+import styles from './Navbar.module.css';
+import {useRouter} from "next/router";
+import Link from "next/link";
+
 type Props = {
   username: string;
 }
 
 const Navbar = ({username}: Props) => {
-  return(
-    <div>
-      Navbar
+  const router = useRouter();
 
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
+  const handleOnclickHome = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push('/');
+  };
+  const handleOnclickMyList = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push('/browse/my-list')
+  };
 
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
-
-          <div>
-            <a>Sign out</a>
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href={'/'}>
+          <div className={styles.logoWrapper}>
+            Logo
           </div>
-        </div>
-      </nav>
+        </a>
+
+        <ul className={styles.navItems}>
+          <li
+            className={styles.navItem}
+            onClick={handleOnclickHome}
+          >Home</li>
+          <li
+            className={styles.navItem2}
+            onClick={handleOnclickMyList}
+          >My List</li>
+        </ul>
+
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <Link href={'/login'} className={styles.linkName}>
+                  Sign out
+                </Link>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
