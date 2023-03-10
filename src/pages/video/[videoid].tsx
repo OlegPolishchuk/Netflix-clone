f00723a0f287106b7fa81aa212045592f9ca6b65
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-03-10 10:38:24 +0300

    feat: add styles for modal

--- src/pages/video/[videoid].tsx
+++ src/pages/video/[videoid].tsx
@@ -9,6 +9,15 @@ const Video = () => {
 
   const {videoid} = router.query;
 
+  const video = {
+    title: 'Hi cute dog',
+    publishTime: '1990-01-01',
+    description: 'A big red dog that is super cute',
+    channelTitle: 'Paramount Pictures',
+    viewCount: 10000,
+  }
+
+  const {channelTitle, title, viewCount, publishTime, description} = video;
 
   return (
     <div className={styles.container}>
@@ -28,6 +37,28 @@ const Video = () => {
             frameBorder={0}
             src={`https://www.youtube.com/embed/${videoid}?autoplay=0&controls=0&origin=http://example.com&rel=0`}
           />
+
+          <div className={styles.modalBody}>
+            <div className={styles.modalBodyContent}>
+              <div className={styles.col1}>
+                <p className={styles.publishTime}>{publishTime}</p>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.description}>{description}</p>
+              </div>
+
+              <div className={styles.col2}>
+                <p className={`${styles.subText} ${styles.subTextWrapper}`}>
+                  <span className={styles.textColor}>Cast: </span>
+                  <span className={styles.channelTitle}>{channelTitle}</span>
+                </p>
+
+                <p className={`${styles.subText} ${styles.subTextWrapper}`}>
+                  <span className={styles.textColor}>View Count: </span>
+                  <span className={styles.channelTitle}>{viewCount}</span>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </Modal>
     </div>
